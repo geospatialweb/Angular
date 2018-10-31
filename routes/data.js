8adb056f362b13b6fd816e88b23055f562ea8c0b
John Campbell <johncampbell@geospatialweb.ca>
2018-10-31 17:09:48 -0400

    refactor nomenclature

--- routes/data.js
+++ routes/data.js
@@ -12,7 +12,7 @@ module.exports =
 
 	const pool = new Pool({
 		/* local instance process.env.DATABASE_URL_LOCAL */
-		connectionString: process.env.DATABASE_URL_LOCAL
+		connectionString: process.env.DATABASE_URL
 	})
 		.on('error', err =>
 		{
